547c7dc27dd3544ce20c25999e6b4da49d0250eb
Tyruiop <archibald.pontier+git@tyruiop.org>
2021-02-25 14:31:18 +0800

    Version bump

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject aho "0.3.0"
+(defproject aho "0.4.0"
   :description "Aho Corasick implementation"
   :url "https://github.com/Tyruiop/aho"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
